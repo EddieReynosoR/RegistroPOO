e6f62064f38f05216febb05d8c1467b42417ad03
YayoReynoso <eduardo_3reynoso@hotmail.com>
2022-06-02 10:38:36 -0700

    Agregar archivos de proyecto.

---
+++ Alumno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Registro
+{
+    //Clase alumno
+    class Alumno
+    {
+        //Atributos
+        private string nombre;
+        private int edad;
+        private string[] materias;
+
+        //Constructor
+        public Alumno(string aNombre, int aEdad, string[] aMaterias)
+        {
+            nombre = aNombre;
+            edad = aEdad;
+            materias = aMaterias;
+        }        
+
+        //Metodo de la clase Alumno
+        public void Registro()
+        {
+            
+            
+            StreamWriter sw = new StreamWriter("alumnos.txt",true);
+
+            /*sw.WriteLine("Nombre \t Edad \t Materias");*/
+
+            sw.Write("\n"+nombre + "\t " + edad + "\t");
+            foreach(string lines in materias)
+            {
+                sw.Write(lines+" || ");
+            }
+
+            
+
+            Console.WriteLine("Se añadieron los datos al archivo.");
+            sw.Close();
+            Console.ReadKey();
+        }
+
+        ~Alumno()
+        {
+            Console.WriteLine("Memoria Liberada Clase Alumno");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Registro
+{
+    //Reynoso Rosales Eduardo Guadalupe #21212036
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string[] asignatures;
+            int cant;
+            try
+            {
+                Console.Write("Indica el nombre del alumno: ");
+                name = Console.ReadLine();
+
+
+                Console.Write("Indica su edad: ");
+                age = int.Parse(Console.ReadLine());
+
+                Console.Write("Cuantas materias esta cursando? ");
+                cant = int.Parse(Console.ReadLine());
+
+                asignatures = new string[cant];
+
+                for (int i = 0; i < asignatures.Length; i++)
+                {
+                    Console.Write("Indica su materia #" + (i + 1) + " : ");
+                    asignatures[i] = Console.ReadLine();
+                }
+
+                Alumno estudiante = new Alumno(name, age, asignatures);
+
+                estudiante.Registro();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Valor ingresado incorrecto!!!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Valor invalido...");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
